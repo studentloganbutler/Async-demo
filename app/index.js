c27ec1e65c1b41c9ef914a56a8f829aa3741d332
Logan Butler <logan.butler@student.swic.edu>
2021-09-22 20:50:54 -0500

    Finish tutorial, add 'test.json'

--- app/index.js
+++ app/index.js
@@ -1,11 +1,22 @@
-import { promises as fs } from "fs";
+import { promises as fs, readFile } from "fs";
+import { writeFile } from "fs/promises";
 
 fs.readFile("./package.json", "utf-8")
   // 'then' is for RESOLVED/FULFILLED promises
   .then((fileContents) => {
-    console.log(fileContents);
-    // Catch is for REJECTED promises
+    fs.writeFile("./test.json", fileContents);
   })
+  .then(() => {
+    console.log(`Finished writing the file.`);
+  })
+  .then(() => {
+    fs.readFile("./test.json", "utf-8");
+  })
+  .then(() => fs.readFile("./test.json", "utf-8"))
+  .then((testFileResults) => {
+    console.log(testFileResults);
+  })
+  // Catch is for REJECTED promises
   .catch((err) => {
     console.error(`Error while reading file, ${err.message}`);
   })
