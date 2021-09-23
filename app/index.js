f51e9856f27f83ada903d572eae21ec7229329a3
Logan Butler <logan.butler@student.swic.edu>
2021-09-22 20:38:12 -0500

    Correct terminology for Tutorial

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import { promises as fs } from "fs";
 
 fs.readFile("./package.json", "utf-8")
-  // 'then' is for RESOLVED promises
+  // 'then' is for RESOLVED/FULFILLED promises
   .then((fileContents) => {
     console.log(fileContents);
     // Catch is for REJECTED promises
@@ -10,7 +10,5 @@ fs.readFile("./package.json", "utf-8")
     console.error(`Error while reading file, ${err.message}`);
   })
   .finally(() => {
-    console.log(
-      "This will run no matter what. This promise has been fulfilled."
-    );
+    console.log("This will run no matter what. This promise has been SETTLED.");
   });
